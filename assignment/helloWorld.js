77608421cba35b3b5857da663bdde8776bc9a092
Kobi Jones <kobijonesmail@gmail.com>
2023-02-20 21:34:01 -0600

    Add a bit of javascript

---
+++ assignment/helloWorld.js
@@ -0,0 +1,18 @@
+const img = document.getElementsByClassName('img')[0];
+const caption = document.getElementsByClassName('img-caption')[0];
+var storedSrc = null;
+var storedCaption = null;
+
+img.onmouseover = function() {
+  storedSrc = img.src;
+  storedCaption = caption.textContent;
+  img.src = 'https://c.tenor.com/4gPD1ccxrVgAAAAC/tenor.gif';
+  caption.textContent = 'Jk';
+}
+
+img.onmouseleave = function() {
+  if (storedSrc !== null)
+    img.src = storedSrc;
+  if (storedCaption !== null)
+    caption.textContent = storedCaption;
+}
