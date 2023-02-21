4f2e2116f06c4e36d15616088fd7d9f88cb70396
Kobi Jones <kobijonesmail@gmail.com>
2023-02-21 17:25:13 -0600

    Add portrait image alt attribute

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -3,18 +3,23 @@ console.log('hello world');
 const img = document.getElementsByClassName('img')[0];
 const caption = document.getElementsByClassName('img-caption')[0];
 var storedSrc = null;
+var storedAlt = null;
 var storedCaption = null;
 
 img.onmouseover = function() {
   storedSrc = img.src;
+  storedAlt = img.alt;
   storedCaption = caption.textContent;
   img.src = 'secret_image.gif';
+  img.alt = 'Rick Astley GIF';
   caption.textContent = 'Jk';
 }
 
 img.onmouseleave = function() {
   if (storedSrc !== null)
     img.src = storedSrc;
+  if (storedAlt !== null)
+    img.alt = storedAlt;
   if (storedCaption !== null)
     caption.textContent = storedCaption;
 }
